391e5d39512446124298dbdc64adf00b4798afb6
gomzyakov <alexander.gomzyakov@gmail.com>
2024-04-13 14:41:26 +0500

    Remove metadata in tests

--- tests/RulesTest.php
+++ tests/RulesTest.php
@@ -3,11 +3,6 @@
 use Gomzyakov\CS\Rules;
 use PHPUnit\Framework\TestCase;
 
-/**
- * @todo Tests are incomplete
- *
- * @covers \Gomzyakov\CS\Rules::getRules
- */
 class RulesTest extends TestCase
 {
     public function test_method_return_array()
